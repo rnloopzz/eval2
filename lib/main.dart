1001a9dc37ca2f98e31c3a06d7b9fe660de00c40
Yordygit config --global user.email yordi.romero22@itca.edu.svgit config --global user.name Yordy <yordi.romero22@itca.edu.sv>
2023-09-29 10:01:25 -0600

    conectando con

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'YordiHD23'),
+      home: const MyHomePage(title: 'YordiHD12'),
     );
   }
 }
